c7265e357010445aef09b69bfdf2af2fb27bfe59
BertramYe <2516559898@qq.com>
2023-08-31 21:04:07 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using SenitronPrintHandlerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
